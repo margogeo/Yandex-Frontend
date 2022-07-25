c5a83df1d223bf9f1c60ebc15b288202b38762e9
Margarita Shimanskaia <shimanskayamargarita@gmail.com>
2022-07-25 06:40:50 +0300

    Update adventure.js

--- JS&TS-course/adventure.js
+++ JS&TS-course/adventure.js
@@ -1,68 +1,100 @@
-function applyMarriageFilter(friends, filter, maxLevel = Infinity) {
-  const bestFriends = friends.filter(friend => friend.best);
-  const queue = [bestFriends];
-  const invited = new Set();
-  let level = 0;
-  while (level < maxLevel && queue.length > 0) {
-    const guests = queue.shift();
-    const following = [];
-    guests
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .forEach(function(person) {
-        if (!invited.has(person)) {
-          invited.add(person);
+'use strict';
 
-          person.friends.forEach(name => following.push(friends.find(p => p.name === name)));
-        }
-      });
+const fetch = require('node-fetch');
 
-    if (following.length > 0) {
-      queue.push(following);
+const API_KEY = require('./key.json');
+
+function getWeather(geoId) {
+
+  function check(weather) {
+    if (weather == 'clear' || weather == 'partly-cloudy') return 's'; 
+    if (weather == 'cloudy' || weather == 'overcast') return 'c';
+    return 'b';
+  }
+   return fetch('https://api.weather.yandex.ru/v2/forecast?geoid='+geoId+'&hours=false&limit=7',
+    {
+      method: 'GET',
+      headers: {
+        'X-Yandex-API-Key': API_KEY.key  
+      }
+    }).then(response => response.json())
+    .catch(error => {throw new Error('Failed to get ' + error);}).then(j => ({id: geoId, weather: j['forecasts'].map(d => check(d['parts']['day_short']['condition']))}))
+    .catch(error => {throw new Error('Unknown error ' + error);});
+}
+
+class TripBuilder {
+  constructor(geoIds) {
+    this.geoIds = geoIds;
+    this.maxim = 100000;
+    this.conds = [];
+  }
+
+  sunny(days) {
+    let i = 0;
+    while (i < days) {
+      i++;
+      this.conds.push('s');
     }
 
-    ++level;
+    return this;
   }
 
-  return [...invited].filter(filter.instance);
-}
-
-function Iterator(friends, filter) {
-  if (!(filter instanceof Filter)) {
-    throw new TypeError('filter must be instance of Filter');
+  cloudy(days) {
+    let i = 0;
+    while (i < days) {
+      i++;
+      this.conds.push('c');
+    }
+    return this;
   }
 
-  this.set = applyMarriageFilter(friends, filter);
+  max(daysCount) {
+    this.maxim = daysCount;
+    return this;
+  }
 
-  this.position = 0;
+  build() {
+    return Promise.all(this.geoIds.map(getWeather)).then(cities => {
+      let cityDay = new Map();
 
-  this.done = () => this.position === this.set.length;
-  this.next = () => (this.done() ? null : this.set[this.position++]);
+      let path = (vis, prev) => {
+
+        if (vis.length === this.conds.length) return vis;
+
+        for (let i = 0; i < cities.length; i++) {
+          let cur;
+          if (cityDay.has(cities[i].id))
+            cur = cityDay.get(cities[i].id);
+          else 
+            cur = 0;
+
+          if (cities[i].weather[vis.length] === this.conds[vis.length] &&(cur === 0 ||(cities[i].id === prev && cur < this.maxim))) {
+            cityDay.set(cities[i].id, cur + 1);
+
+            let end = path(vis.concat({ geoid: cities[i].id, day: vis.length + 1 }), cities[i].id);
+
+            if (end) return end;
+
+            cityDay.set(cities[i].id, cur);
+          }
+        }
+
+        return null;
+      };
+
+      let result = path([], -1);
+
+      if (result) return result;
+
+      throw new Error('Не могу построить маршрут!');
+    });
+  }
 }
 
-function LimitedIterator(friends, filter, maxLevel) {
-  Iterator.call(this, friends, filter);
-  this.set = applyMarriageFilter(friends, filter, maxLevel);
+function planTrip(geoids) {
+  return new TripBuilder(geoids);
 }
-LimitedIterator.prototype = Object.create(Iterator.prototype, {constructor: { value: LimitedIterator }});
-
-function Filter() {
-  this.instance = () => true;
-}
-
-function MaleFilter() {
-  this.instance = friend => friend.gender === 'male';
-}
-MaleFilter.prototype = Object.create(Filter.prototype, {constructor: { value: MaleFilter }});
-
-function FemaleFilter() {
-  this.instance = friend => friend.gender === 'female';
-}
-FemaleFilter.prototype = Object.create(Filter.prototype, {constructor: { value: FemaleFilter }});
 
 module.exports = {
-  Iterator,
-  Filter,
-  LimitedIterator,
-  MaleFilter,
-  FemaleFilter
+  planTrip
 };
